bef8b80a723eec2715fee99b4d168df455db07f6
annaliseriaz <felicityzoe23@gmail.com>
2026-02-14 14:02:09 +0800

    Maulan.java

---
+++ Maulan.java
@@ -0,0 +1,20 @@
+package wala_lang;
+import java.util.Scanner;
+public class Maulan {
+
+	public static void main(String[] args) {
+		Scanner labyu = new Scanner(System.in);
+		int input;
+		System.out.print("Ilang i love you gusto mo? : ");
+		input=labyu.nextInt();
+		int ctr = 0;
+		
+		do
+		{
+			System.out.println("I love youuuuuu");
+			ctr--;
+		}
+		while (ctr<input);
+		System.out.print("UwU");
+	}
+}
